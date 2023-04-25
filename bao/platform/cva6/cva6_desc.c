aa9ef76c26eca1913007550ecb1e2c265f01ec8c
Bruno Sa <bruno.vilaca.sa@gmail.com>
2023-04-26 00:59:01 +0100

    add bao cva6 platform files
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

---
+++ bao/platform/cva6/cva6_desc.c
@@ -0,0 +1,24 @@
+#include <platform.h>
+#include <arch/plic.h>
+
+struct platform platform = {
+
+    .cpu_num = 1,
+
+    .region_num = 1,
+    .regions =  (struct mem_region[]) {
+        {
+            .base = 0x80200000,
+            .size = 0x40000000 - 0x200000
+        }
+    },
+
+    .console = {
+        .base = 0x10000000,
+    },
+
+    .arch = {
+        .plic_base = 0xc000000,
+    }
+
+};
---
+++ bao/platform/cva6/platform.mk
@@ -0,0 +1,12 @@
+# Architecture definition
+ARCH:=riscv
+# CPU definition
+CPU:=
+
+drivers := 8250_uart
+
+platform_description:=cva6_desc.c
+platform-cppflags =
+platform-cflags = 
+platform-asflags =
+platform-ldflags =
