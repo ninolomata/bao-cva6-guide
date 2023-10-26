544fdcd67ff4d11be4b2e7275e2dbf6ad1409f5e
Bruno Sa <bruno.vilaca.sa@gmail.com>
2023-10-26 16:01:29 +0100

    add culsans bao config files
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

---
+++ bao/configs/culsans-baremetal/config.c
@@ -0,0 +1,48 @@
+#include <config.h>
+
+VM_IMAGE(baremetal_image, XSTR(/path/to/bao-baremetal-guest/build/culsans/baremetal.bin));
+
+struct config config = {
+    
+    CONFIG_HEADER
+
+    .vmlist_size = 1,
+    .vmlist = {
+        { 
+            .image = {
+                .base_addr = 0x80200000,
+                .load_addr = VM_IMAGE_OFFSET(baremetal_image),
+                .size = VM_IMAGE_SIZE(baremetal_image)
+            },
+
+            .entry = 0x80200000,
+
+            .platform = {
+                .cpu_num = 2,
+                
+                .region_num = 1,
+                .regions =  (struct vm_mem_region[]) {
+                    {
+                        .base = 0x80200000,
+                        .size = 0x4000000
+                    }
+                },
+
+                .dev_num = 1,
+                .devs =  (struct vm_dev_region[]) {
+                    {
+                        .pa = 0x10000000,   
+                        .va = 0x10000000,  
+                        .size = 0x00010000,  
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {1}
+                    }
+                },
+
+                .arch = {
+                   .irqc.plic.base = 0xc000000,
+                }
+            },
+        },
+     }
+};
---
+++ bao/configs/culsans-dual-vm/config.c
@@ -0,0 +1,106 @@
+#include <config.h>
+
+VM_IMAGE(linux_image, XSTR(/path/to/bao-cva6-guide/linux/lloader/linux-rv64-culsans-one.bin));
+VM_IMAGE(baremetal_image, XSTR(/path/to/bao-cva6-guide/bao-baremetal-guest/build/culsans/baremetal.bin));
+
+struct config config = {
+    
+    CONFIG_HEADER
+
+    .vmlist_size = 2,
+    .vmlist = {
+        { 
+            .image = {
+                .base_addr = 0x80200000,
+                .load_addr = VM_IMAGE_OFFSET(linux_image),
+                .size = VM_IMAGE_SIZE(linux_image)
+            },
+
+            .entry = 0x80200000,
+
+            .platform = {
+                .cpu_num = 1,
+                
+                .region_num = 1,
+                .regions =  (struct vm_mem_region[]) {
+                    {
+                        .base = 0x80200000,
+                        .size = 0x20000000
+                    }
+                },
+
+                .dev_num = 4,
+                .devs =  (struct vm_dev_region[]) {
+                    {
+                        .pa = 0x18000000,   
+                        .va = 0x18000000,  
+                        .size = 0x00001000,  
+                        .interrupt_num = 4,
+                        .interrupts = (irqid_t[]) {4,5,6,7}
+                    },
+                    {
+                        .pa = 0x20000000,   
+                        .va = 0x20000000,  
+                        .size = 0x00001000,  
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {2}
+                    },
+                    {
+                        .pa = 0x30000000,   
+                        .va = 0x30000000,  
+                        .size = 0x00008000,  
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {3}
+                    },
+                    {
+                        .pa = 0x40000000,   
+                        .va = 0x40000000,  
+                        .size = 0x00010000,  
+                        .interrupt_num = 0,
+                        .interrupts = (irqid_t[]) {}
+                    },
+                },
+
+                .arch = {
+                   .irqc.plic.base = 0xc000000,
+                }
+            },
+        },
+        { 
+            .image = {
+                .base_addr = 0x80200000,
+                .load_addr = VM_IMAGE_OFFSET(baremetal_image),
+                .size = VM_IMAGE_SIZE(baremetal_image)
+            },
+
+            .entry = 0x80200000,
+
+            .platform = {
+                .cpu_num = 1,
+                
+                .region_num = 1,
+                .regions =  (struct vm_mem_region[]) {
+                    {
+                        .base = 0x80200000,
+                        .size = 0x4000000
+                    }
+                },
+
+                .dev_num = 1,
+                .devs =  (struct vm_dev_region[]) {
+                    {
+                        .pa = 0x10000000,   
+                        .va = 0x10000000,  
+                        .size = 0x00010000,  
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {1}
+                    }
+                },
+
+                .arch = {
+                   .irqc.plic.base = 0xc000000,
+                }
+            },
+        },
+     }
+};
